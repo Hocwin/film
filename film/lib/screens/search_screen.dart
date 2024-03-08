afcabf54fd0e2e3b165e9af54b982bb1ad6ff3a1
Hocwin <146694692+Hocwin@users.noreply.github.com>
2024-03-08 08:47:21 +0700

    first

---
+++ film/lib/screens/search_screen.dart
@@ -0,0 +1,124 @@
+import 'package:film/models/movie.dart';
+import 'package:film/screens/detail_screen.dart';
+import 'package:film/services/api_services.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SearchScreen> {
+  final ApiService _apiService = ApiService();
+  final TextEditingController _searchController = TextEditingController();
+  List<Movie> _searchResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_SearchMovie);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  void _SearchMovie() async {
+    if (_searchController.text.isEmpty) {
+      setState(() {
+        _searchResults.clear();
+      });
+      return;
+    }
+    final List<Map<String, dynamic>> searchData =
+        await _apiService.searchMovie(_searchController.text);
+
+    setState(() {
+      _searchResults = searchData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        hintText: 'Search movies...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    visible: _searchController.text.isNotEmpty,
+                    child: IconButton(
+                      onPressed: () {
+                        _searchController.clear();
+                        setState(() {
+                          _searchResults.clear();
+                        });
+                      },
+                      icon: const Icon(Icons.clear),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _searchResults.length,
+                itemBuilder: (context, index) {
+                  final Movie movie = _searchResults[index];
+
+                  return ListTile(
+                    leading: Image.network(
+                      movie.posterPath != ''
+                          ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
+                          : 'https://via.placeholder.com/50x75?text=No+Image',
+                      height: 50,
+                      width: 50,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(movie.title),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailScreen(movie: movie)));
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
